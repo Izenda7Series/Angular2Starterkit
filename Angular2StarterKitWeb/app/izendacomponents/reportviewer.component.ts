9b63eb9ed95c79c818da41073b10001122d0b4da
Stanislav Bukin <bukin.stanislav@gmail.com>
2019-12-21 14:48:08 +0500

    IZ-25648: Added support for subreports.

--- Angular2StarterKitWeb/app/izendacomponents/reportviewer.component.ts
+++ Angular2StarterKitWeb/app/izendacomponents/reportviewer.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import {IzendaIntegrate} from '../_helpers/izendaintegrate';
-
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     templateUrl: 'rootcontainer.html'
@@ -9,10 +9,24 @@ import {IzendaIntegrate} from '../_helpers/izendaintegrate';
 export class ReportViewer implements AfterViewInit, OnDestroy {
     dom: any = {};
 
-    constructor(private izItergrate: IzendaIntegrate) {}
+    constructor(private izItergrate: IzendaIntegrate, private activatedRoute: ActivatedRoute) {}
 
     ngAfterViewInit() {
-        this.dom = this.izItergrate.RenderReportViewer();
+        let reportId = null;
+        this.activatedRoute.params.subscribe((params: Params) => {
+            reportId = params['id'];
+        });
+
+        let filters = null;
+        this.activatedRoute.queryParams.subscribe((params: Params) => {
+            const overridingFilterValue = {};
+            Object.keys(params).forEach(function(key) {
+                overridingFilterValue[key] = decodeURIComponent(params[key]);
+            });
+            filters = { overridingFilterValue: overridingFilterValue };
+        });
+
+        this.dom = this.izItergrate.RenderReportViewer(reportId, filters);
         this.izItergrate.AutoHideIzenaProgressBar();
     }
     
